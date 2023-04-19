d42e2dc677a450e9b4f62b48b98a68750dc7f987
Mihir <60245851+Mihir611@users.noreply.github.com>
2023-04-19 10:27:38 +0000

    Added the frontend code

---
+++ frontend/ERC20_Minter/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { browser } from "$app/environment";
+    import { ethers } from "ethers";
+
+    let connected = false;
+    let address = "";
+    let balance = "";
+    let network = "";
+
+    let tokenName = "";
+    let symbol = "";
+    let deployedAddr = "";
+
+</script>
+<div class="box">
+    <div>
+        <h1>Welcome to ERC20 Token Minter</h1>
+        <br />
+        <p>This Minter gives the demo of how to mint an ERC20 token on a testnet</p>
+        <p>
+            Please follow the below mentioned steps and mint your 1<sup>st</sup> token
+            on the blockchain
+        </p>
+        <input type="button" value="Connect Wallet" />
+    </div>
+</div>
+
+<style>
+    .box {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+</style>
